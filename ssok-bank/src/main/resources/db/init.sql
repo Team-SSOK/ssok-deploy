9265f8baac8682b36cab8eb8915fe2fac4d87863
구동혁 <dhku@naver.com>
2025-04-30 16:52:46 +0900

    fix: init.sql 수정

--- ssok-bank/src/main/resources/db/init.sql
+++ ssok-bank/src/main/resources/db/init.sql
@@ -1,4 +1,4 @@
-INSERT INTO good (name, account_type_code, interest_rate, interest_cycle)
+INSERT INTO good (created_at, updated_at, name, account_type_code, interest_rate, interest_cycle)
 VALUES
-('기본 예금 상품', 'DEPOSIT', 1.5, 30),
-('기본 적금 상품', 'SAVINGS', 2.0, 30);
+(NOW(), NOW(), '기본 예금 상품', 'DEPOSIT', 1.5, 30),
+(NOW(), NOW(), '기본 적금 상품', 'SAVINGS', 2.0, 30);
