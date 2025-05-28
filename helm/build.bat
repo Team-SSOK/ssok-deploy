c28be8c7d5ecc34782a41df9bc53961a49c3fbbf
dhku <dhku@naver.com>
2025-05-28 21:10:40 +0900

    feat: helm 자동화 빌드 및 dry-run 배치파일 추가

---
+++ helm/build.bat
@@ -0,0 +1,16 @@
+@Echo off
+
+helm package ./ssok-kafka
+helm package ./ssok-bank
+helm package ./ssok-bank-proxy
+helm package ./ssok-account-service
+helm package ./ssok-bluetooth-service
+helm package ./ssok-gateway-service
+helm package ./ssok-notification-service
+helm package ./ssok-transfer-service
+helm package ./ssok-user-service
+helm package ./ssok-openbanking
+
+helm repo index .
+
+pause
