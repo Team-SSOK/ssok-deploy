70ebddc9eb85e86d0f8a50c5775bd05cb94a2547
isasgp <isasgp@naver.com>
2025-06-11 12:33:49 +0900

    fix: ssom-llm 젠킨스 빌드 수정

--- jenkins/ssom-llm/deploy.sh
+++ jenkins/ssom-llm/deploy.sh
@@ -9,7 +9,6 @@ separationPhrase="=====================================";
 
 DOCKER_NICKNAME="kudong"
 BACKEND_IMAGE_NAME="ssom-llm"
-BACKEND_DB_IMAGE_NAME="ssom-qdrantdb"
 DEPLOY_NAME="ssok-deploy"
 TAG="1.$BUILD_NUMBER"
 BUILD="jenkins"
@@ -26,7 +25,6 @@ echo "remoteID = $remoteID"
 echo "remoteIP = $remoteIP" 
 echo "DOCKER_NICKNAME = $DOCKER_NICKNAME" 
 echo "BACKEND_IMAGE_NAME = $BACKEND_IMAGE_NAME"
-echo "BACKEND_DB_IMAGE_NAME = $BACKEND_DB_IMAGE_NAME"
 echo "DEPLOY_NAME = $DEPLOY_NAME"
 echo "TAG = $TAG"
 echo
@@ -50,11 +48,9 @@ echo "OpenAI API Key 주입 완료"
 #백엔드 도커 이미지 빌드
 cd $currentDir/ssom_server
 docker buildx build -t $BACKEND_IMAGE_NAME:$TAG .
-docker buildx build -t $BACKEND_DB_IMAGE_NAME:$TAG .
 
 #백엔드 도커 파일 tar 저장
 docker save -o $currentDir/images/$BACKEND_IMAGE_NAME.tar $BACKEND_IMAGE_NAME:$TAG
-docker save -o $currentDir/images/$BACKEND_DB_IMAGE_NAME.tar $BACKEND_DB_IMAGE_NAME:$TAG
 
 #도커 허브에 업로드
 echo $separationPhrase
@@ -76,22 +72,15 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 cd $currentDir
 docker tag "$BACKEND_IMAGE_NAME:$TAG" "$DOCKER_NICKNAME/$BACKEND_IMAGE_NAME:latest"
 docker tag "$BACKEND_IMAGE_NAME:$TAG" "$DOCKER_NICKNAME/$BACKEND_IMAGE_NAME:$TAG"
-docker tag "$BACKEND_DB_IMAGE_NAME:$TAG" "$DOCKER_NICKNAME/$BACKEND_DB_IMAGE_NAME:latest"
-docker tag "$BACKEND_DB_IMAGE_NAME:$TAG" "$DOCKER_NICKNAME/$BACKEND_DB_IMAGE_NAME:$TAG"
 
 #허브 이미지 푸시
 docker push "$DOCKER_NICKNAME/$BACKEND_IMAGE_NAME:latest"
 docker push "$DOCKER_NICKNAME/$BACKEND_IMAGE_NAME:$TAG"
-docker push "$DOCKER_NICKNAME/$BACKEND_DB_IMAGE_NAME:latest"
-docker push "$DOCKER_NICKNAME/$BACKEND_DB_IMAGE_NAME:$TAG"
 
 #허브 이미지 제거
 docker image rmi $DOCKER_NICKNAME/$BACKEND_IMAGE_NAME:latest
 docker image rmi $DOCKER_NICKNAME/$BACKEND_IMAGE_NAME:$TAG
 docker image rmi $BACKEND_IMAGE_NAME:$TAG
-docker image rmi $DOCKER_NICKNAME/$BACKEND_DB_IMAGE_NAME:latest
-docker image rmi $DOCKER_NICKNAME/$BACKEND_DB_IMAGE_NAME:$TAG
-docker image rmi $BACKEND_DB_IMAGE_NAME:$TAG
 docker image prune -f
 
 
