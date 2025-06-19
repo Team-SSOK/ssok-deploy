14eaefe4f661640a5881197f7869017269a49505
구동혁 <dhku@naver.com>
2025-06-19 13:34:55 +0900

    fix: argocd install.sh 수정

--- jenkins/argocd/install.sh
+++ jenkins/argocd/install.sh
@@ -6,12 +6,23 @@
 
 bcrypt='$2a$12$CiFnLWgFppLX62AYL.2NvOhDUswoVW.1JPoIZuLuY8BxTIQ/aEkKu' # ssok0414!
 
+# 설치후 kubectl get svc -n argocd 확인
 helm install argocd argo/argo-cd \
-  -n argocd \
-  --create-namespace \
-  --set-string configs.secret.argocdServerAdminPassword="$bcrypt" \
-  --set server.env[0].name="TZ" \
-  --set server.env[0].value="Asia/Seoul"
+   -n argocd \
+   --create-namespace \
+   --set server.service.type=LoadBalancer \
+   --set-string configs.secret.argocdServerAdminPassword="$bcrypt" \
+   --set server.env[0].name="TZ" \
+   --set server.env[0].value="Asia/Seoul"
+
+# helm install argocd argo/argo-cd \
+#    -n argocd \
+#    --create-namespace \
+#    --set server.service.type=LoadBalancer \
+#    --set-string configs.secret.argocdServerAdminPassword="$bcrypt" \
+#    --set server.env[0].name="TZ" \
+#    --set server.env[0].value="Asia/Seoul"
+
 
 
 
